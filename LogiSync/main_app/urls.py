5723ef4acf2048c243e57ebd7635c261d2629793
10Fawaz <faaljar2@gmail.com>
2025-08-26 14:43:31 +0300

    made the home page

--- LogiSync/main_app/urls.py
+++ LogiSync/main_app/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     
     
     path('containers/', views.ContainerList.as_view(), name='container_list'),
-    path('containers/<int:pk>/', views.ContainerDetail.as_view(), name='container_detail' ),
+    path('containers/<str:pk>/', views.ContainerDetail.as_view(), name='container_detail' ),
     
     path('containers/create/', views.ContainerCreate.as_view(), name='container_create' ),
-    path('containers/<int:pk>/update/', views.ContainerUpdate.as_view(), name='container_update' ),
-    path('containers/<int:pk>/delete/', views.ContainerDelete.as_view(), name='container_delete' ),
+    path('containers/<str:pk>/update/', views.ContainerUpdate.as_view(), name='container_update' ),
+    path('containers/<str:pk>/delete/', views.ContainerDelete.as_view(), name='container_delete' ),
+    
 ]
