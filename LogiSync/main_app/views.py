d73a3fd8701f00aa9e189bf21089b20194ef07b3
abdkhalid24 <busaud973@gmail.com>
2025-08-26 14:59:53 +0300

    TransportUpdate class is maked

--- LogiSync/main_app/views.py
+++ LogiSync/main_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import TransportType
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 # Create your views here.
 class TransportCreate(CreateView):
     model = TransportType
     fields = '__all__'
+
+class TransportUpdate(UpdateView):
+    models = TransportType
+    fields = code
 
 
 
