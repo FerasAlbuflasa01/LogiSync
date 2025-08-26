9797d623144ed56e64d41579be5334adbedbb1d8
abdkhalid24 <busaud973@gmail.com>
2025-08-26 14:57:26 +0300

    TransportCreate class is maked

--- LogiSync/main_app/views.py
+++ LogiSync/main_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import TransportType
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+class TransportCreate(CreateView):
+    model = TransportType
+    fields = '__all__'
+
+
 
