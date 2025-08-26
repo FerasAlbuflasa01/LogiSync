acbba6f8f0849f72f0aab2f6c75f09e6c73ee1f7
abdkhalid24 <busaud973@gmail.com>
2025-08-26 14:22:41 +0300

    TransportType model is created in models.py

--- LogiSync/main_app/models.py
+++ LogiSync/main_app/models.py
@@ -22,6 +22,11 @@ class Transport(models.Model):
     source = models.ForeignKey(Source, on_delete=models.CASCADE)
     # CODE = models.CharField(max_length=20)    
 
+class TransportType(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='main_app/static/uploads/', default='')
+
 
     def get_absolute_url(self):
         return reverse("detail", kwargs={'transport_id': self.id})
